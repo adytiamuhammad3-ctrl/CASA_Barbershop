c66b26ed0c5ff31088cbe90ad9e8b06fff15bd46
adytiamuhammad3-ctrl <adytiamuhammad3@gmail.com>
2025-12-26 22:55:20 +0700

    inc

---
+++ inc/header.php
@@ -0,0 +1,29 @@
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light p-3">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/img/logo.png" alt="Logo" width="38" height="35"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+            <li class="nav-item">
+              <a
+                href="./login.php"
+                class="nav-link btn btn-color-theme ps-3 pe-3"
+                >Admin</a
+              >
+            </li>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar End -->
